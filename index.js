ecffe5a75adc71be8c9119b4dae13f09f7435b0e
shajanjp <shajanjp@gmail.com>
2021-01-11 21:06:46 +0530

    fix: adds config and activity commands

--- index.js
+++ index.js
@@ -3,22 +3,34 @@ const { config } = require("process");
 const HOME_DIR = require("os").homedir();
 const fs = require("fs");
 const fsp = fs.promises;
-let DATA_DIR = "";
+const DATA_DIR = HOME_DIR + "/.ijust";
+let repoGit;
 
 (async () => {
-  if (process.argv.length == 3) {
+  await initGit();
+
+  if (process.argv.length > 2) {
     switch (process.argv[2]) {
       case "init": {
-        await createDirectory(HOME_DIR + "/.ijust");
-        DATA_DIR = HOME_DIR + "/.ijust";
-        git(DATA_DIR).init((data) => {
-          console.log({ data });
-        });
+        await initGit();
+
         break;
       }
+
       case "config": {
+        console.log(process.argv);
+        if (
+          process.argv.length > 5 &&
+          process.argv[3] === "remote" &&
+          process.argv[4] === "add" &&
+          !(await repoGit.getRemotes()).map((r) => r.name).includes("origin")
+        ) {
+          await repoGit.addRemote("origin", process.argv[5]);
+        }
+
         break;
       }
+
       default: {
         await processActivity(process.argv[2]);
       }
@@ -26,12 +38,49 @@ let DATA_DIR = "";
   }
 })();
 
+async function initGit() {
+  await createDirectory(DATA_DIR);
+
+  repoGit = git(DATA_DIR).init();
+}
+
 async function createDirectory(path) {
   if (!fs.existsSync(path)) {
     return fsp.mkdir(path, { recursive: true });
   }
 }
 
+async function pushDataToFile(fileName, activityData) {
+  const filePath = `${DATA_DIR}/${fileName}`;
+
+  return new Promise((resolve, reject) => {
+    fs.access(filePath, fs.constants.F_OK, async (err) => {
+      if (!err) {
+        const fileData = JSON.parse(await fsp.readFile(filePath, "utf8"));
+
+        fileData.push(activityData);
+        await fsp.writeFile(filePath, JSON.stringify(fileData), "utf8");
+      } else {
+        await createDirectory(DATA_DIR);
+        await fsp.writeFile(filePath, JSON.stringify([activityData]), "utf8");
+      }
+      resolve();
+    });
+  });
+}
+
 async function processActivity(activity) {
-    
+  const now = new Date();
+  const fileName = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}.json`;
+  
+  await pushDataToFile(`${fileName}`, {
+    activity,
+    timespamp: new Date(),
+  });
+  try {
+    await repoGit
+      .add([`${DATA_DIR}/${fileName}`])
+      .commit(`activity: ${activity}`)
+      .push(["-u", "origin", "master"], () => {});
+  } catch (error) {}
 }
